4599b9b73e9c33688c9f19628b8fbd92e158b7f5
padelarif <arifpadel04@gmail.com>
2026-04-15 00:34:15 +0800

    Add files via upload

---
+++ if-else.py
@@ -0,0 +1,15 @@
+# if
+# else
+# elif
+
+nama = input("masukkan nama")
+NIM = input("masukkan PIN NIM")
+
+if nama =="padelkids" :
+    NIM =="01"
+    print("Hai saya padelkids")
+    print("NIM saya 25241084")
+else : 
+    print("Mohon maaf anda bukan padelkids")
+    print("NIM anda tidak bisa diakses")    
+print("program selesai")
