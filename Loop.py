67d4d57a98962376fa7fd15e2fdf38c971f95669
padelarif <arifpadel04@gmail.com>
2026-04-18 03:34:47 +0800

    Add files via upload

---
+++ Loop.py
@@ -0,0 +1,11 @@
+# perulangan (LOOP)
+# for kondisi :
+
+angka = -1
+
+while angka < 1 :
+    angka = int(input("masukkan angka:"))
+    if angka < 1:
+        print("Harus positif")
+
+print(f"Angka di terima: {angka}")
